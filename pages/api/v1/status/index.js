ea2985bc24b710b773bcf4d43221e099e1983b00
Kelvin Fernandes <sokelvinfernandes@gmail.com>
2025-11-04 23:44:22 -0300

    Add jsconfig.json

--- pages/api/v1/status/index.js
+++ pages/api/v1/status/index.js
@@ -1,9 +1,8 @@
-import database from '../../../../infra/database.js'
+import database from '/infra/database.js'
 
 async function status(request, response) {
   const dbResult = await database.query("SELECT \'OK\' AS message;");
-  console.log('Database response:', dbResult);
-  response.status(200).json({ status: 'OK' });
+  response.status(200).json({ status: dbResult });
 }
 
 export default status;
