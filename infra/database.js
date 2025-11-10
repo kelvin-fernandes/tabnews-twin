81712d110ef75e378eef4900a451ac3a69070f45
Kelvin Fernandes <sokelvinfernandes@gmail.com>
2025-11-10 20:22:28 -0300

    Refactor SSL configuration in database connection

--- infra/database.js
+++ infra/database.js
@@ -8,7 +8,7 @@ async function connect() {
       database: process.env.POSTGRES_DB,
       user: process.env.POSTGRES_USER,
       password: process.env.POSTGRES_PASSWORD,
-      ssl: process.env.NODE_ENV === 'production',
+      ssl: getSSLValues(),
     });
     await client.connect();
     return client;
@@ -34,6 +34,13 @@ async function getDatabaseInfo() {
   finally {
     await client.end();
   }
+}
+
+function getSSLValues() {
+  if (process.env.POSTGRES_CA)
+    return { ca: process.env.POSTGRES_CA };
+
+  return process.env.NODE_ENV === 'production'
 }
 
 export default { getDatabaseInfo };
