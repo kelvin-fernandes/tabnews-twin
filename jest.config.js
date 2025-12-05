76d6bd6f42fbf765ee51858b74d006555b3dd87b
Kelvin Fernandes <sokelvinfernandes@gmail.com>
2025-12-04 21:52:11 -0300

    Refactor tests to use centralized setup for service readiness

--- jest.config.js
+++ jest.config.js
@@ -4,6 +4,7 @@ const createJestConfig = nextJest();
 const jestConfig = createJestConfig({
   moduleDirectories: ["node_modules", "<rootDir>"],
   globalSetup: "<rootDir>/dotenv-test.js",
+  setupFilesAfterEnv: ["<rootDir>/jest.setup.js"],
   testTimeout: 30000,
 });
 
--- tests/integration/api/v1/migrations/get.test.js
+++ tests/integration/api/v1/migrations/get.test.js
@@ -1,11 +1,9 @@
 import database from "infra/database.js";
-import orchestrator from "tests/orchestrator.js"
 
 let response;
 let responseData;
 
 beforeAll(async () => {
-  await orchestrator.waitForAllServices();
   await database.query("DROP SCHEMA public CASCADE; CREATE SCHEMA public;");
   response = await fetch("http://localhost:3000/api/v1/migrations");
   responseData = await response.json();
