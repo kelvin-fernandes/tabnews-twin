a7a4a1bca309f039c50ec3e6443f3884777d9e49
Kelvin Fernandes <sokelvinfernandes@gmail.com>
2025-12-09 00:01:08 -0300

    Refactor tests to use orchestrator for service readiness and add async-retry dependency (#14)
    * Refactor tests to use orchestrator for service readiness and add async-retry dependency
    
    * Refactor tests to use centralized setup for service readiness

--- jest.config.js
+++ jest.config.js
@@ -4,6 +4,8 @@ const createJestConfig = nextJest();
 const jestConfig = createJestConfig({
   moduleDirectories: ["node_modules", "<rootDir>"],
   globalSetup: "<rootDir>/dotenv-test.js",
+  setupFilesAfterEnv: ["<rootDir>/jest.setup.js"],
+  testTimeout: 30000,
 });
 
 module.exports = jestConfig;
---
+++ tests/orchestrator.js
@@ -0,0 +1,22 @@
+import retry from "async-retry"
+
+async function waitForAllServices() {
+  await waitForWebServer()
+
+  async function waitForWebServer() {
+    await retry(async () => {
+      const response = await fetch("http://localhost:3000/api/v1/statuss")
+
+      if (!response.ok) {
+        throw new Error("Web server not ready")
+      }
+    }, {
+      retries: 50,
+      maxTimeout: 1000,
+    })
+  }
+}
+
+export default {
+  waitForAllServices,
+}
