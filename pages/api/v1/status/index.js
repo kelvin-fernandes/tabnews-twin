3bc4dc03fac363dfba3bbc72c83368e34b79f73c
Kelvin Fernandes <sokelvinfernandes@gmail.com>
2026-01-31 00:53:55 -0300

    feat: restructure error handling in api status endpoint

--- pages/api/v1/status/index.js
+++ pages/api/v1/status/index.js
@@ -1,19 +1,25 @@
 import database from "/infra/database.js";
+import { InternalServerError } from "/infra/errors.js";
 
 async function status(request, response) {
-  const updatedAt = new Date().toISOString();
-  const databaseInfo = await database.getDatabaseInfo();
-  response.status(200).json({
-    updated_at: updatedAt,
-    dependencies: {
-      database: {
-        status: "healthy",
-        version: databaseInfo.dbVersion,
-        max_connections: parseInt(databaseInfo.maxConnections),
-        opened_connections: parseInt(databaseInfo.openedConnections),
+  try {
+    const updatedAt = new Date().toISOString();
+    const databaseInfo = await database.getDatabaseInfo();
+    response.status(200).json({
+      updated_at: updatedAt,
+      dependencies: {
+        database: {
+          status: "healthy",
+          version: databaseInfo.dbVersion,
+          max_connections: parseInt(databaseInfo.maxConnections),
+          opened_connections: parseInt(databaseInfo.openedConnections),
+        },
       },
-    },
-  });
+    });
+  } catch (error) {
+    const errorResponse = new InternalServerError({ cause: error });
+    response.status(500).json(errorResponse);
+  }
 }
 
 export default status;
