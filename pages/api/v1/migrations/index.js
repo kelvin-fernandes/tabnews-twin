5541722423482ae51fa01faeab2a34d35cc8c154
Kelvin Fernandes <sokelvinfernandes@gmail.com>
2026-02-08 20:16:20 -0300

    refactor: simplify migration handling by integrating migrator model

--- pages/api/v1/migrations/index.js
+++ pages/api/v1/migrations/index.js
@@ -1,8 +1,6 @@
 import { createRouter } from "next-connect";
-import migrationRunner from "node-pg-migrate";
-import { resolve } from "node:path";
-import database from "infra/database.js";
 import controller from "infra/controller.js";
+import migrator from "models/migrator.js";
 
 const router = createRouter();
 
@@ -12,47 +10,11 @@ router.post(postHandler);
 export default router.handler(controller.errorHandlers);
 
 async function getHandler(request, response) {
-  let dbClient;
-
-  try {
-    dbClient = await getDatabaseConnection();
-    const defaultMigrationsOptions = getDefaultMigrationsOptions(dbClient);
-
-    const pendingMigrations = await migrationRunner(defaultMigrationsOptions);
-    return response.status(200).json(pendingMigrations);
-  } finally {
-    await dbClient.end();
-  }
+  const pendingMigrations = await migrator.getPendingMigrations();
+  return response.status(200).json(pendingMigrations);
 }
 
 async function postHandler(request, response) {
-  let dbClient;
-
-  try {
-    dbClient = await getDatabaseConnection();
-    const defaultMigrationsOptions = getDefaultMigrationsOptions(dbClient);
-
-    const migratedMigrations = await migrationRunner({
-      ...defaultMigrationsOptions,
-      dryRun: false,
-    });
-    return response.status(201).json(migratedMigrations);
-  } finally {
-    await dbClient.end();
-  }
-}
-
-async function getDatabaseConnection() {
-  return await database.connect();
-}
-
-function getDefaultMigrationsOptions(dbClient) {
-  return {
-    dryRun: true,
-    dbClient: dbClient,
-    dir: resolve("infra", "migrations"),
-    direction: "up",
-    verbose: true,
-    migrationsTable: "pgmigrations",
-  };
+  const migratedMigrations = await migrator.runPendingMigrations();
+  return response.status(201).json(migratedMigrations);
 }
