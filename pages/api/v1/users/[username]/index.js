f2ed538dbb40b5fdebc10bfd3fb1be0d8bc79ca6
Kelvin Fernandes <sokelvinfernandes@gmail.com>
2026-03-11 22:43:30 -0300

    feat: add patch handler for updating user information

--- pages/api/v1/users/[username]/index.js
+++ pages/api/v1/users/[username]/index.js
@@ -5,6 +5,7 @@ import user from "models/user.js";
 const router = createRouter();
 
 router.get(getHandler);
+router.patch(patchHandler);
 
 export default router.handler(controller.errorHandlers);
 
@@ -12,6 +13,15 @@ async function getHandler(request, response) {
   const usernameInput = request.query.username;
 
   var userResponse = await user.findOneByUsername(usernameInput);
+
+  return response.status(200).json(userResponse);
+}
+
+async function patchHandler(request, response) {
+  const username = request.query.username;
+  const userInputValues = request.body;
+
+  var userResponse = await user.update(username, userInputValues);
 
   return response.status(200).json(userResponse);
 }
