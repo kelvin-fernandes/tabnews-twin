9c762de3141a9441f79146846ce2834415716283
Kelvin Fernandes <sokelvinfernandes@gmail.com>
2026-03-18 19:53:51 -0300

    Features/hash password (#34)
    * build: add bcryptjs dependency for password hashing
    
    * feat: implement password model
    
    * feat: implement password hashing in user create method
    
    * refactor: improve formatting and logging in password hashing functions
    
    * feat: add update functionality for user model with validation and password hashing
    
    * feat: add patch handler for updating user information
    
    * chore: add BLACK_PEPPER variable to development environment configuration

---
+++ models/password.js
@@ -0,0 +1,34 @@
+import bcryptjs from "bcryptjs";
+
+async function hash(password) {
+  const rounds = process.env.NODE_ENV === "production" ? 14 : 1;
+  const hashedPassword = await bcryptjs.hash(
+    password + getBlackPepper(),
+    rounds,
+  );
+  return hashedPassword;
+}
+
+async function compare(password, hash) {
+  const isValidPassword = await bcryptjs.compare(
+    password + getBlackPepper(),
+    hash,
+  );
+  return isValidPassword;
+}
+
+function getBlackPepper() {
+  const blackPepper = process.env.BLACK_PEPPER;
+  if (!blackPepper) {
+    throw new Error("BLACK_PEPPER environment variable is not set.");
+  }
+
+  return blackPepper;
+}
+
+const password = {
+  hash,
+  compare,
+};
+
+export default password;
--- tests/integration/api/v1/users/[username]/get.test.js
+++ tests/integration/api/v1/users/[username]/get.test.js
@@ -6,6 +6,11 @@ let getResponse2;
 let getResponse2Data;
 let response404;
 let response404Data;
+const userInputBody = {
+  username: "getKF1",
+  email: "getkf@pm.me",
+  password: "pass",
+};
 
 async function sendPostRequest(inputBody) {
   return await fetch("http://localhost:3000/api/v1/users", {
@@ -25,16 +30,12 @@ beforeAll(async () => {
   await orchestrator.clearDatabase();
   await orchestrator.runPendingMigrations();
 
-  await sendPostRequest({
-    username: "KF1",
-    email: "kf@pm.me",
-    password: "pass",
-  });
+  await sendPostRequest(userInputBody);
 
-  getResponse1 = await sendGetRequest("KF1");
+  getResponse1 = await sendGetRequest(userInputBody.username);
   getResponse1Data = await getResponse1.json();
 
-  getResponse2 = await sendGetRequest("kf1");
+  getResponse2 = await sendGetRequest("getkf1");
   getResponse2Data = await getResponse2.json();
 
   response404 = await sendGetRequest("unexisting_user");
@@ -51,8 +52,11 @@ describe("GET /api/v1/users/[username]", () => {
       test("should return the expected user data", async () => {
         expect(getResponse1Data).toEqual({
           id: expect.any(String),
-          username: "KF1",
-          email: "kf@pm.me",
+          username: userInputBody.username,
+          email: userInputBody.email,
+          password: getResponse1Data.password,
+          created_at: getResponse1Data.created_at,
+          updated_at: getResponse1Data.updated_at,
         });
       });
     });
@@ -65,8 +69,11 @@ describe("GET /api/v1/users/[username]", () => {
       test("should return the expected user data", async () => {
         expect(getResponse2Data).toEqual({
           id: expect.any(String),
-          username: "KF1",
-          email: "kf@pm.me",
+          username: userInputBody.username,
+          email: userInputBody.email,
+          password: getResponse2Data.password,
+          created_at: getResponse2Data.created_at,
+          updated_at: getResponse2Data.updated_at,
         });
       });
     });
