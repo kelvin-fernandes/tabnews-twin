b5aa35f7f09cb2e62dd2da23126d85cf8b0ddea2
Kelvin Fernandes <sokelvinfernandes@gmail.com>
2026-05-09 23:13:48 -0300

    feat: implement `/user` endpoint with session validation and renewal

---
+++ pages/api/v1/user/index.js
@@ -0,0 +1,22 @@
+import { createRouter } from "next-connect";
+import controller from "infra/controller.js";
+import session from "models/session.js";
+import user from "models/user.js";
+
+const router = createRouter();
+
+router.get(getHandler);
+
+export default router.handler(controller.errorHandlers);
+
+async function getHandler(request, response) {
+  const token = request.cookies.session_id;
+
+  const validSession = await session.findValidSessionToken(token);
+  const renewedSession = await session.renew(validSession.id);
+  controller.setSessionCookie(response, renewedSession.token);
+
+  const userFromSession = await user.findOneById(validSession.user_id);
+
+  return response.status(200).json(userFromSession);
+}
